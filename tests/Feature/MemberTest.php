7a93a13b0d05b6d835bc37de3ba96f60637fe6ec
hee <emop99@naver.com>
2022-06-22 20:41:32 +0900

    Member Register TestCase

---
+++ tests/Feature/MemberTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class MemberTest extends TestCase
+{
+    /**
+     * 회원 가입 Test
+     * @return void
+     */
+    public function test_member_register()
+    {
+        User::where('email', 'test@naver.com')->delete();
+
+        $response = $this->post('/api/auth/member/register', [
+            'name'                  => 'test',
+            'email'                 => 'test@naver.com',
+            'password'              => 'test12',
+            'password_confirmation' => 'test12',
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
